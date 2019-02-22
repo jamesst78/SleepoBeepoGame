99b8ad0965b6185ccd13db1b773da7cc376854dd
mohad <mohad@DESKTOP-2VJ1VES>
2019-02-22 23:15:03 +0200

    GasControl unit done

---
+++ OurCsGameFinal/src/model/units/GasControlUnit.java
@@ -0,0 +1,11 @@
+package model.units;
+import simulation.*;
+
+public class GasControlUnit extends FireUnit {
+
+	
+	public GasControlUnit(String id , Address location , 
+			int stepsPerCycle) {
+		super(id , location , stepsPerCycle);
+	}
+}
