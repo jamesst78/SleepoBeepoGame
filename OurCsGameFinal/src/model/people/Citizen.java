6867cc41066c370e9feff7241c516db0ab3e93d5
mohad <mohad@DESKTOP-2VJ1VES>
2019-02-22 22:03:37 +0200

    update

---
+++ OurCsGameFinal/src/model/people/Citizen.java
@@ -0,0 +1,19 @@
+package model.people;
+
+import simulation.*;
+
+public class Citizen implements Simulatable , Rescuable  {
+
+	private CitizenState state ;
+	private Disaster disaster;
+	private Address location ;
+	private String nationalID;
+	private String name;
+	private int age;
+	
+	
+	
+	
+	
+	
+}
--- OurCsGameFinal/src/simulation/Address.java
+++ OurCsGameFinal/src/simulation/Address.java
@@ -2,14 +2,24 @@ package simulation;
 
 public class Address {
 
-		int x;
-		int y;
+		private int x;
+		private int y;
 		
 		
 	public Address(int x , int y) {
 		this.x = x;
 		this.y =y;
 }
+
+
+	public int getX() {
+		return x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
 	
 }
 
