ce08fb4dd53bd148976eca91ba889286e8f1d9e8
mohad <mohad@DESKTOP-2VJ1VES>
2019-02-22 22:48:34 +0200

    UNit fully done

--- OurCsGameFinal/src/model/units/Unit.java
+++ OurCsGameFinal/src/model/units/Unit.java
@@ -1,7 +1,61 @@
 package model.units;
-
+import simulation.*;
 abstract public class Unit {
 		String unitID;
 		UnitState state;
+		Address location;
+		Rescuable target;
+		int distanceToTarget;
+		int stepsPerCycle;
 		
+		
+		
+	public Unit(String id , Address location , int stepsPerCycle) {
+		this.unitID = id;
+		this.location = location;
+		this.stepsPerCycle = stepsPerCycle;
+		this.state = UnitState.IDLE;
+	}
+
+
+
+	public UnitState getState() {
+		return state;
+	}
+
+
+
+	public void setState(UnitState state) {
+		this.state = state;
+	}
+
+
+
+	public Address getLocation() {
+		return location;
+	}
+
+
+
+	public void setLocation(Address location) {
+		this.location = location;
+	}
+
+
+
+	public String getUnitID() {
+		return unitID;
+	}
+
+
+
+	public int getStepsPerCycle() {
+		return stepsPerCycle;
+	}
+
+
+
+	public void setTarget(Rescuable target) {
+		this.target = target;
+	}
 }
