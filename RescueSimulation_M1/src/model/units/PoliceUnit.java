fa2689c1a89d2da094e7a72f4fa894102680e1c3
mohad <mohad@DESKTOP-2VJ1VES>
2019-03-24 21:36:44 +0200

    evacuator is done

--- RescueSimulation_M1/src/model/units/PoliceUnit.java
+++ RescueSimulation_M1/src/model/units/PoliceUnit.java
@@ -13,6 +13,8 @@ public abstract class PoliceUnit extends Unit {
 	private ArrayList<Citizen> passengers;
 	private int maxCapacity;
 	private int distanceToBase;
+	private boolean ToGoBackToBuilding;
+	private boolean  ToGoBackToBase;
 
 	public PoliceUnit(String unitID, Address location, int stepsPerCycle, int maxCapacity , WorldListener toAdd) {
 
@@ -36,7 +38,92 @@ public abstract class PoliceUnit extends Unit {
 	
 	
 	public void cycleStep() {
+		ResidentialBuilding x = (ResidentialBuilding)this.getTarget();
 		
+		if(this.getState().equals(UnitState.RESPONDING)) {
+			for(int i = 0 ; i<x.getOccupants().size() ; i++) {
+				if(!x.getOccupants().get(i).getState().equals(CitizenState.DECEASED)) {
+					break;
+				}
+				else {
+					this.jobsDone();
+					return;
+				}
+			}
+			
+			int distance = x.getLocation().getX() + x.getLocation().getY();
+			this.setDistanceToBase(distance);
+			if(distance<=0) {
+				this.setLocation(x.getLocation());
+				//dlw2ty 27na wasalna for the first time. We need to load up the citizens and make sure TO CHECK B3DAHA if there are any left. Also turn state into treating and call treat method
+				this.setState(UnitState.TREATING);
+				this.treat();
+				this.ToGoBackToBase = true;
+			}
+			else {
+			distance = distance - this.getStepsPerCycle();
+			}
+			
+			
+		if(this.getState().equals(UnitState.TREATING) && this.ToGoBackToBase ) {
+				
+				int distance2 = this.getDistanceToBase();
+				if(distance2<=0) {
+					this.getWorldListener().assignAddress(this, 0, 0);
+					//big shit
+					for(int i = 0 ; i<this.passengers.size() ; i++) { //bafady el citizens
+							this.passengers.get(i).getWorldListener().assignAddress(this.passengers.get(i), 0, 0);
+							this.passengers.get(i).setState(CitizenState.RESCUED);
+							this.passengers.remove(i);
+					}
+						this.ToGoBackToBase = false; //saying eno ana 5las 5lst el base boolean
+						if(x.getOccupants().size() == 0){
+							this.ToGoBackToBuilding = false;
+							this.jobsDone();
+							return;
+						}
+							
+					for(int i = 0 ; i<x.getOccupants().size() ; i++) { //checking if I have a reason to go back
+						if(!x.getOccupants().get(i).getState().equals(CitizenState.DECEASED)) {
+							this.ToGoBackToBuilding = true; //if i have a reason , set its boolean eny raye7 tany to the building
+							break;
+						
+						}
+						else {
+							this.ToGoBackToBuilding = false;
+							this.jobsDone();
+										
+						}
+								
+					}
+				}
+					else {
+						distance2 = distance2 - this.getStepsPerCycle();
+					}
+					
+			}
+			
+			if(this.getState().equals(UnitState.TREATING) && this.ToGoBackToBuilding ) {
+				
+				int distance3 = x.getLocation().getX() + x.getLocation().getY();
+				if(distance3 <=0) {				
+					this.treat(); //3abeet
+					this.ToGoBackToBuilding = false;
+					this.ToGoBackToBase = true;
+					
+									
+				}
+				else {
+					distance3 = distance3-this.getStepsPerCycle();
+					
+				}
+					
+				
+			}
+			
+		}
+		
+						
 		
 		//Law el unit responding
 		//a7aded hal howa ray7 wla rag3?
@@ -46,49 +133,28 @@ public abstract class PoliceUnit extends Unit {
 		//el treat should load citizens on the car
 		//BA2ET el cycle step , should deliver the citizens to the base / back to the location
 		//once Im back ANDDDD theres a flag , en ana lesa m5lst4 , 
-		//I'll call treat again
+		//I'll call treat again 
 	}
+
 
 
 	
 	public void treat() {
 		if(this.getTarget() instanceof ResidentialBuilding) {
 			
-			ResidentialBuilding x = (ResidentialBuilding)this.getTarget();
-			int distance = x.getLocation().getX() + x.getLocation().getY();
-			this.setDistanceToBase(distance); 
-			boolean AllDead = true;
+		ResidentialBuilding x = (ResidentialBuilding)this.getTarget();
+		
+		if(!x.getOccupants().isEmpty()) {
 			for(int i = 0 ; i<x.getOccupants().size() ; i++) {
 				if(!x.getOccupants().get(i).getState().equals(CitizenState.DECEASED)) {
-					AllDead = false;
-				}
-				if(AllDead) {
-					this.jobsDone();
-					return;
-				}
-			}
-			
-			for(int j = 0 ; j<x.getOccupants().size() && passengers.size()< maxCapacity ; j++) {
-				if(!x.getOccupants().get(j).getState().equals(CitizenState.DECEASED))
-				passengers.add(x.getOccupants().get(j));
-			}
-			if(this.passengers.size() == this.maxCapacity) {
-				if(distance <=0) {
-					this.getWorldListener().assignAddress(this,0,0);
-					//Hanfady el 3arabeya hena lazm n7ot listeners lel citizens el awel.
-					if(!x.getOccupants().isEmpty()) {
-						
-					}
-					if(x.getOccupants().isEmpty()) {
-						this.jobsDone();
-					}
+					passengers.add(x.getOccupants().get(i));  //7ateeto fl 3rbya
+					x.getOccupants().remove(i);    //removed mn el occupants
 					
-				}
-				else {
-					distance = distance - this.getStepsPerCycle();
 				}
 				
 			}
+		}
+		
 				
 			
 		}
--- RescueSimulation_M1/src/tests/testingMainMethod.java
+++ RescueSimulation_M1/src/tests/testingMainMethod.java
@@ -1,10 +1,23 @@
 package tests;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class testingMainMethod {
 public static void main(String[] args) {
 	
+	
+	ArrayList r = new ArrayList<>();
+	int i = 1;
+	r.add(5);
+	r.add(4);
+	r.add(2);
+	int y = (int) r.get(i);
+	System.out.println(r);
+	for(int k = 0 ;k<r.size() ; k++) {
+		System.out.println(r.get(k) + " ");
+	
+	}
 	
 }
 }
